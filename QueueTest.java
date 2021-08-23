4afb12b1752821dd258095621619c610d6711c3b
Ivan Garcia-Sanchez <60237177+Ivan-Garcia-S@users.noreply.github.com>
2021-08-23 15:13:13 -0700

    Update QueueTest.java
    Added description

--- QueueTest.java
+++ QueueTest.java
@@ -1,13 +1,14 @@
+//Test client for Queue ADT
 public class QueueTest {
 	public static void main(String args[]) {
 	
 	Queue q = new Queue();
 	Job j = new Job(4,5);
-	//Node n = new Node(j);
+	//	Node n = new Node(j);
 	
 	System.out.println(q.isEmpty());
 	q.enqueue(j);
-//	System.out.println(q.peek());
+	//	System.out.println(q.peek());
 	q.enqueue(new Job(6,2));
 	q.enqueue(new Job(10,1));
 	System.out.println(q.length());
@@ -15,9 +16,9 @@ public class QueueTest {
 	System.out.println(q.isEmpty());
 	
 	q.dequeueAll();
-//	q.dequeue();
-//	q.dequeue();
-//	System.out.println(q.toString());
+	//	q.dequeue();
+	//	q.dequeue();
+	//	System.out.println(q.toString());
 	System.out.println(q.isEmpty());
 	
 	
